b47b3feb7bad89767c771a74723ce812596772fd
Sergio Villanueva <sergiovillanueva@protonmail.com>
2025-09-08 23:06:52 -0300

    Initial commit

---
+++ server/src/main/java/ar/sergiovillanueva/chronomed/ChronomedApplication.java
@@ -0,0 +1,13 @@
+package ar.sergiovillanueva.chronomed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronomedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronomedApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/ar/sergiovillanueva/chronomed/ChronomedApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.sergiovillanueva.chronomed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronomedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

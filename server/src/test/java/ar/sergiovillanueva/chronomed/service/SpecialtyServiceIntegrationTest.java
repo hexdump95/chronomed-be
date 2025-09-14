6f30f61e299038fc0db55a1029737a4e32594a07
Sergio Villanueva <sergiovillanueva@protonmail.com>
2025-09-14 12:31:41 -0300

    Fix application-test.yaml

--- server/src/test/java/ar/sergiovillanueva/chronomed/service/SpecialtyServiceIntegrationTest.java
+++ server/src/test/java/ar/sergiovillanueva/chronomed/service/SpecialtyServiceIntegrationTest.java
@@ -10,6 +10,7 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Instant;
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = ChronomedApplication.class)
+@ActiveProfiles("test")
 @Transactional
 class SpecialtyServiceIntegrationTest {
 

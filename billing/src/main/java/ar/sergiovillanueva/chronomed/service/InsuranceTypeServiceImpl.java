07af11f0563b0ff131ab0841ba88d208b5f39a65
Sergio Villanueva <sergiovillanueva@protonmail.com>
2025-09-30 23:23:05 -0300

    Update InsuranceType.getAll, sort by name

--- billing/src/main/java/ar/sergiovillanueva/chronomed/service/InsuranceTypeServiceImpl.java
+++ billing/src/main/java/ar/sergiovillanueva/chronomed/service/InsuranceTypeServiceImpl.java
@@ -1,10 +1,12 @@
 package ar.sergiovillanueva.chronomed.service;
 
 import ar.sergiovillanueva.chronomed.dto.SelectEntityResponse;
+import ar.sergiovillanueva.chronomed.entity.InsuranceType_;
 import ar.sergiovillanueva.chronomed.repository.InsuranceTypeRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,7 +27,8 @@ public class InsuranceTypeServiceImpl implements InsuranceTypeService {
     @Transactional(readOnly = true)
     public List<SelectEntityResponse> findAllInsuranceTypes() {
         log.debug("Find all insuranceTypes");
-        var insuranceTypes = insuranceTypeRepository.findAll();
+        var sort = Sort.by(Sort.Direction.ASC, InsuranceType_.name.getName());
+        var insuranceTypes = insuranceTypeRepository.findAll(sort);
         return objectMapper.convertValue(insuranceTypes, objectMapper.getTypeFactory().constructCollectionType(List.class, SelectEntityResponse.class));
     }
 }
